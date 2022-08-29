821cede5e41eeb66ddb41b77465529c658ad1db9
Maaz Bin Hossain <mhossainsaki@gmail.com>
2022-08-30 01:13:06 +0600

    final Backup and initialization of other scenes

--- src/main/java/com/example/uiuconnect/YourthoughtsPanel.java
+++ src/main/java/com/example/uiuconnect/YourthoughtsPanel.java
@@ -1,4 +1,29 @@
 package com.example.uiuconnect;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class YourthoughtsPanel {
+    @FXML
+    private Button PostButton;
+
+    @FXML
+    private TextArea postShow;
+
+    @FXML
+    private TextField postTextField;
+
+    @FXML
+    void PostButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void posttextFieldAction(ActionEvent event) {
+
+    }
+
 }
