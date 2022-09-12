17352517d57957ecdd0dd951c0e11370545d3b99
Kamrul-hassan1438 <98039269+Kamrul-hassan1438@users.noreply.github.com>
2022-09-12 23:41:50 +0600

    Chitchat

---
+++ src/main/java/Massage/Information.java
@@ -0,0 +1,11 @@
+package Massage;
+
+public class Information {
+    public String username;
+    public NetworkConnection netConnection;
+    public Information(String user, NetworkConnection nConnection){
+        username=user;
+        netConnection=nConnection;
+    }
+
+}
---
+++ src/main/java/Massage/NetworkConnection.java
@@ -0,0 +1,47 @@
+package Massage;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class NetworkConnection {
+    Socket socket;
+    ObjectInputStream ois;
+    ObjectOutputStream oos;
+
+    public NetworkConnection(Socket sock) throws IOException {
+        socket=sock;
+        oos=new ObjectOutputStream(socket.getOutputStream());
+        ois=new ObjectInputStream(socket.getInputStream());
+    }
+
+    public NetworkConnection(String ip, int port) throws IOException{
+        socket=new Socket(ip, port);
+        oos=new ObjectOutputStream(socket.getOutputStream());
+        ois=new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void write(Object obj){
+        try {
+            oos.writeObject(obj);
+        } catch (IOException ex) {
+            System.out.println("Failed to write");
+            //throw ex;
+        }
+    }
+
+    public Object read(){
+        Object obj;
+        try {
+            obj = ois.readObject();
+        } catch (Exception ex) {
+            System.out.println("Failed to read");
+            return null;
+        }
+        return obj;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+}
